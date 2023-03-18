b8d7873ae1fb434e0f23b016c46c4434d3c7464f
Rashid Rasheed <rashidrasheed81@gmail.com>
2023-03-19 00:15:39 +0500

    nothing change

--- my-resume/src/app/page.tsx
+++ my-resume/src/app/page.tsx
@@ -1,7 +1,5 @@
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
-import Footer from "./components/footer/Footer";
-import Header from "./components/header/Header";
 import HomeHero from "./components/HomeHero";
 import Projects from "./components/projects/Projects";
 
